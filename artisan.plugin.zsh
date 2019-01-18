e69f3b3d0023806482ad6eb6ab8983ce147df960
Luuk van Kooten <luukvankooten@gmail.com>
2019-01-18 21:56:02 +0100

    initial commit

---
+++ artisan.plugin.zsh
@@ -0,0 +1,49 @@
+#
+# Aliases
+# 
+#
+
+alias artisan='php artisan'
+
+alias a='php artisan'
+
+alias at ='php artisan tinker'
+
+alias amake='php artisan make:'
+
+alias ammo='php artisan make:model'
+
+alias ammi='php artisan make:middleware'
+
+alias ammig='php artisan make:migration'
+
+alias amp ='php artisan make:provider'
+
+alias amr='php artisan make:rule'
+
+alias amc='php artisan make:controller'
+
+alias amt='php artisan make:test'
+
+alias amigf='php artisan migrate:fresh'
+
+alias amigi='php artisan migrate:install'
+
+alias amigref='php artisan migrate:refresh'
+
+alias amigres='php artisan migrate:reset'
+
+alias amigrol='php artisan migrate:rollback'
+
+alias amigsta='php artisan migrate:status'
+
+alias adbs='php artisan db:seed'
+
+alias aca='php artisan cache'
+
+alias acac='php artisan cache:clear'
+
+alias acaf='php artisan cache:forget'
+
+alias acat='php artisan cache:table'
+
